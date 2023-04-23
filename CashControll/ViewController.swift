11a6810b1bdcb2996d64dcebbd3165adaf259dd0
kistalex <kistalexander@yandex.ru>
2023-04-23 21:18:29 +0400

    Initial Commit

---
+++ CashControll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CashControll
+//
+//  Created by Александр Кисть on 23.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
